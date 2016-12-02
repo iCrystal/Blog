26b427be5b549b753d4afd70edbd00a938bc2637
e <1367687435@qq.com>
2016-12-03 00:56:09 +0800

    x

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'Vue-resource'
+
 import App from './App'
+import Home from './components/Home'
+import 'bootstarp/dist/css'
 
 /* eslint-disable no-new */
 new Vue({
