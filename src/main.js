9b5a1325548b95cd6ab79c9c07dd2b7b7da0559f
e <1367687435@qq.com>
2016-12-03 19:54:04 +0800

    任务表

--- src/main.js
+++ src/main.js
@@ -1,14 +1,61 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
-import VueResource from 'Vue-resource'
+import 'bootstrap/dist/css/bootstrap.css'
 
+import store from './store'
 import App from './App'
+
+import NotFound from './components/404'
 import Home from './components/Home'
-import 'bootstarp/dist/css'
+import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : 'home',
+  component : Home
+},{
+  path : '/time-entries',
+  component : TimeEntries,
+  children : [{
+    path : 'log-time',
+    //懒加载
+    component : LogTime,
+  }]
+},{
+  path : '*',
+  component : NotFound
+}];
+
+const router = new VueRouter({
+  routes
+});
 
 /* eslint-disable no-new */
-new Vue({
+// 这灵活得亮瞎了
+/*new Vue({
   el: '#app',
   template: '<App/>',
+  router,
   components: { App }
-})
+});
+ new Vue(Vue.util.extend({
+   router
+ }, App)).$mount('#app');
+new Vue({
+  el:'#app',
+  router,
+  render:h => h(App)
+});*/
+var app = new Vue({
+  el: '#app',
+  router,
+  store,
+  ...App,
+});
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+// src/store/index.js 完整代码
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './action'
+
+Vue.use(Vuex);
+
+const state = {
+  totalTime: 0,
+  list: []
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+})
